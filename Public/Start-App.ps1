4f801eb6249adea6d770df3c6b327502cd75e004
prayzzz <prayzzz@outlook.com>
2017-08-26 15:55:33 +0200

    - return instead of exit

--- Public/Start-App.ps1
+++ Public/Start-App.ps1
@@ -8,7 +8,7 @@ Function Start-App {
 
     if (!(Test-Path $AppExec)) {
         Write-Error "App not found"
-        exit 1
+        return 1
     }
 
     # start process
--- Public/Stop-App.ps1
+++ Public/Stop-App.ps1
@@ -9,7 +9,7 @@ Function Stop-App {
 
     if (!(Test-Path $PidFile)) {
         Write-Warning "$PidFile not found"
-        exit 0;
+        return 0;
     }
     
     # read process id
