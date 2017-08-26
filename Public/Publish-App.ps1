4379a973b7f0f1a421ee941d369bb02373e077ef
prayzzz <prayzzz@outlook.com>
2017-08-26 16:31:08 +0200

    - fix

--- Public/Publish-App.ps1
+++ Public/Publish-App.ps1
@@ -3,28 +3,28 @@ Function Publish-App {
     [CmdletBinding()]
     Param(
         [Parameter(Position = 0, Mandatory = $true)] [string] $Artifact,
-        [Parameter(Position = 1, Mandatory = $true)] [string] $AppExec
+        [Parameter(Position = 1, Mandatory = $true)] [string] $DeployTo,
+        [Parameter(Position = 2, Mandatory = $true)] [string] $AppExec
     )
 
+    $AppExecPath = Join-Path $DeployTo $AppExec
+
     Write-Host "Stop running instance..."
-    Stop-App -App $AppExec
+    Stop-App $AppExecPath
 
     
     Write-Host "Expanding new version..."
-    $Folder = (Get-Item $Artifact).Basename    
-    If (Test-Path $Folder) {
-        Remove-Item $Folder -Recurse
+    If (Test-Path $DeployTo) {
+        Remove-Item $DeployTo -Recurse
     }    
     $ProgressPreference = "SilentlyContinue"
-    Expand-Archive -Path $Artifact -DestinationPath $Folder -Force
+    Expand-Archive -Path $Artifact -DestinationPath $DeployTo -Force
+    Remove-Item $Artifact
 
     Write-Host "Start instance..."    
-    $AppExecPath = Join-Path $Folder $AppExec
-    
     If ($ENV:OS -Ne "Windows_NT") {
         Write-Host "Apply file permission"
         chmod 755 $AppExecPath        
     }
-
     Start-App $AppExecPath
 }
